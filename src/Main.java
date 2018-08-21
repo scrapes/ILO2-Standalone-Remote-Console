3a97c4055686b6e28f769c4a6c956f4f6619c065
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-08-21 18:25:52 +0200

    Main: fixed derp

--- src/Main.java
+++ src/Main.java
@@ -98,7 +98,7 @@ public class Main {
         SSLUtilities.trustAllHostnames();
         SSLUtilities.trustAllHttpsCertificates();
 
-        URL obj = new URL(loginURL);
+        URL obj = new URL("https://" + hostname + "/index.htm");
 
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
