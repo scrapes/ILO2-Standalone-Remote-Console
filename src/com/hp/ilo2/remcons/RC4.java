66ad6d23325d9b85960f4ad5ce7e54eb3c6e36d5
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-12-28 11:06:27 +0100

    RC4: cleanup, naming

--- src/com/hp/ilo2/remcons/RC4.java
+++ src/com/hp/ilo2/remcons/RC4.java
@@ -1,31 +1,20 @@
 package com.hp.ilo2.remcons;
 
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class RC4
-{
 
+public class RC4 {
 
+  private byte[] key = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  private byte[] pre = new byte[16];
+  private byte[] sBox = new byte[0x100];
+  private byte[] keyBox = new byte[0x100];
+  private int i = 0;
+  private int j = 0;
 
-
-
-
-
-
-
-
-  byte[] key = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-  byte[] pre = new byte[16];
-  byte[] sBox = new byte['Ā'];
-  byte[] keyBox = new byte['Ā'];
-  int i = 0;
-  int j = 0;
-
-  RC4(byte[] paramArrayOfByte)
-  {
-    System.arraycopy(paramArrayOfByte, 0, this.pre, 0, 16);
+  RC4(byte[] key) {
+    System.arraycopy(key, 0, this.pre, 0, 16);
       try {
           update_key();
       } catch (NoSuchAlgorithmException e) {
@@ -33,31 +22,21 @@ public class RC4
       }
   }
 
-
-
   void update_key() throws NoSuchAlgorithmException {
     MessageDigest md = MessageDigest.getInstance("MD5");
     md.update(this.pre);
     md.update(this.key);
 
-    byte[] arrayOfByte = md.digest();
-    System.arraycopy(arrayOfByte, 0, this.key, 0, this.key.length);
+    byte[] digest = md.digest();
+    System.arraycopy(digest, 0, this.key, 0, this.key.length);
 
-
-
-
-
-
-    for (int k = 0; k < 256; k++)
-    {
+    for (int k = 0; k < 256; k++) {
       this.sBox[k] = ((byte)(k & 0xFF));
       this.keyBox[k] = this.key[(k % 16)];
     }
 
-
     this.j = 0;
-    for (this.i = 0; this.i < 256; this.i += 1)
-    {
+    for (this.i = 0; this.i < 256; this.i += 1) {
       this.j = ((this.j & 0xFF) + (this.sBox[this.i] & 0xFF) + (this.keyBox[this.i] & 0xFF) & 0xFF);
       int m = this.sBox[this.i];
       this.sBox[this.i] = this.sBox[this.j];
@@ -68,21 +47,13 @@ public class RC4
     this.j = 0;
   }
 
-  int randomValue()
-  {
+  int randomValue() {
     this.i = ((this.i & 0xFF) + 1 & 0xFF);
     this.j = ((this.j & 0xFF) + (this.sBox[this.i] & 0xFF) & 0xFF);
     int k = this.sBox[this.i];
     this.sBox[this.i] = this.sBox[this.j];
     this.sBox[this.j] = (byte)k;
     int m = (this.sBox[this.i] & 0xFF) + (this.sBox[this.j] & 0xFF) & 0xFF;
-    int n = this.sBox[m];
-    return n;
+    return (int) this.sBox[m];
   }
 }
-
-
-/* Location:              C:\Users\anton\Documents\ILO2\rc175p10.jar!\com\hp\ilo2\remcons\RC4.class
- * Java compiler version: 4 (48.0)
- * JD-Core Version:       0.7.1
- */
