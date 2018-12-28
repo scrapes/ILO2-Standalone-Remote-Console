7db14853e7ac2a55b37cdd5d2623f0bc8abe15d1
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-12-28 11:13:01 +0100

    MouseSyncListener: removed decompiler comment

--- src/com/hp/ilo2/remcons/MouseSyncListener.java
+++ src/com/hp/ilo2/remcons/MouseSyncListener.java
@@ -9,9 +9,3 @@ interface MouseSyncListener {
 
     void serverClick(int button, int paramInt2);
 }
-
-
-/* Location:              C:\Users\anton\Documents\ILO2\rc175p10.jar!\com\hp\ilo2\remcons\MouseSyncListener.class
- * Java compiler version: 4 (48.0)
- * JD-Core Version:       0.7.1
- */
