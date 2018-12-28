6151daa78674ea00942807641be9822b3f30a4b2
fridtjof <2780577+fridtjof@users.noreply.github.com>
2018-12-28 11:11:30 +0100

    TimerListener: removed decompiler comment

--- src/com/hp/ilo2/remcons/TimerListener.java
+++ src/com/hp/ilo2/remcons/TimerListener.java
@@ -3,8 +3,3 @@ package com.hp.ilo2.remcons;
 interface TimerListener {
     void timeout(Object callbackInfo);
 }
-
-/* Location:              C:\Users\anton\Documents\ILO2\rc175p10.jar!\com\hp\ilo2\remcons\TimerListener.class
- * Java compiler version: 4 (48.0)
- * JD-Core Version:       0.7.1
- */
